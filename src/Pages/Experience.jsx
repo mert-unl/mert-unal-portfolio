25d50cf6a03556a3ae2c940e1e007dd6f0d65ff8
Mert ÜNAL <mertunal1998@gmail.com>
2025-11-14 00:06:04 +0300

    experiences page - dynamic

--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -3,43 +3,37 @@ import { AppContext } from '../context/AppContextProvider'
 
 function Experience() {
 
-   
-  const  = useContext(AppContext)
+  const {langData}  = useContext(AppContext)
+  const experiences = langData.experiencesSection.experiences
 
   return (
-    <div className='flex flex-col'>
-   
-     <h2 className="text-4xl md:text-6xl font-medium text-center  animate-blink  py-2  ">
-        Tecrübelerim
-      </h2>
-<div className=' md:grid md:grid-cols-3 flex flex-col gap-6 mt-5'>
-  <ExperienceElement/>
-   <ExperienceElement/>
-   <ExperienceElement/>
-   <ExperienceElement/>
-   <ExperienceElement/>
-   <ExperienceElement/>
-</div>
- 
-    </div>
+      <div className='flex flex-col'>
+      
+        <h2 className="text-4xl md:text-6xl font-medium text-center  animate-blink  py-2  ">
+          {langData.experiencesSection.title}
+        </h2>
+
+        <div className=' md:grid md:grid-cols-3 flex flex-col gap-6 mt-5'>
+            {experiences.map((item)=>  <ExperienceElement key={item.id} company={item.name} position={item.position} year={item.years} context={item.context} />)} 
+        </div>
+      
+      </div>
   )
 }
 
 
 
-function ExperienceElement(){
-
-
-    return(
-        <div className='flex flex-col border border-lime-800 backdrop-blur-xs bg-black/10 hover:scale-108 duration-1000 transition-all hover:shadow-green-300 hover:shadow-xl shadow-green-800 shadow-lg rounded-xl p-4'>
-            <p className='text-3xl font-bold'>Insider</p>
+function ExperienceElement({key,company,position,year,context}){
+    return (
+        <div id={key} className='flex flex-col border border-lime-800 backdrop-blur-sm bg-black/10 hover:scale-108 duration-1000 transition-all hover:shadow-green-300 hover:shadow-xl shadow-green-800 shadow-lg rounded-xl p-4'>
+            <p className='text-3xl font-bold'>{company}</p>
             
             <div className='flex flex-row  py-1 justify-between'>
-                <p className="text-lg text-green-300  font-semibold">Bootcamp</p>
-                <p>2016-2020</p>
+                <p className="text-lg text-green-300  font-semibold">{position}</p>
+                <p>{year}</p>
             </div>
     
-            <p className='py-2 text-md leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat accusamus voluptatem, quod assumenda hic necessitatibus, minima nam unde cum atque doloribus molestias exercitationem aliquam numquam ut ratione! Iusto, ducimus enim.</p>
+            <p className='py-2  text-md  break-words'>{context}</p>
         </div>
     )
 }
